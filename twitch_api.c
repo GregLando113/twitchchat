d80c5500bf7a41a18d6376054748a934362ac56c
Gorg <gorg@Gorgs-MacBook.local>
2017-10-18 00:25:58 -0700

    fixed indents

--- twitch_api.c
+++ twitch_api.c
@@ -38,260 +38,260 @@ extern "C" {
 #define TWITCH_CHAT_API_ROOT "irc.chat.twitch.tv"
 #define TWITCH_CHAT_API_PORT "6667"
     
-    // Utility functions //
-    char* append_msg(char* buf,char* msg)
+// Utility functions //
+char* append_msg(char* buf,char* msg)
+{
+    for(;*msg;++buf,++msg)
     {
-        for(;*msg;++buf,++msg)
-        {
-            *buf = *msg;
-        }
-        return buf;
+        *buf = *msg;
     }
-    char* finalize_msg(char* buf)
+    return buf;
+}
+char* finalize_msg(char* buf)
+{
+    // NOTE: May not need null terminator, check on this
+    buf[0] = '\r';
+    buf[1] = '\n';
+    return buf + 2;
+}
+
+void parse_irc(twitch_ircmessage* msg, char* raw)
+{
+    memset(msg,0,sizeof(twitch_ircmessage));
+    char msgfound = 0;
+    char* pivot = raw;
+    if(*pivot == ':')
     {
-        // NOTE: May not need null terminator, check on this
-        buf[0] = '\r';
-        buf[1] = '\n';
-        return buf + 2;
+        msg->isResponse = 1;
+        ++pivot; ++raw;
     }
-    
-    void parse_irc(twitch_ircmessage* msg, char* raw)
+    for(;*(short*)raw != '\r\n';++raw)
     {
-        memset(msg,0,sizeof(twitch_ircmessage));
-        char msgfound = 0;
-        char* pivot = raw;
-        if(*pivot == ':')
+        if (msgfound)
         {
-            msg->isResponse = 1;
-            ++pivot; ++raw;
+            continue;
         }
-        for(;*(short*)raw != '\r\n';++raw)
+        if(*raw == ':')
         {
-            if (msgfound)
-            {
-                continue;
-            }
-            if(*raw == ':')
+            *raw = '\0';
+            msg->params[msg->paramc++] = raw + 1;
+            msgfound = 1;
+        }
+        else if(*raw == '!')
+        {
+            *raw = '\0';
+            msg->nick = pivot;
+            pivot = raw + 1;
+        }
+        else if(*raw == '@')
+        {
+            *raw = '\0';
+            msg->user = pivot;
+            pivot = raw + 1;
+        }
+        else if(*raw == ' ')
+        {
+            if(msg->host == NULL)
             {
                 *raw = '\0';
-                msg->params[msg->paramc++] = raw + 1;
-                msgfound = 1;
-            }
-            else if(*raw == '!')
-            {
-                *raw = '\0';
-                msg->nick = pivot;
+                msg->host = pivot;
                 pivot = raw + 1;
             }
-            else if(*raw == '@')
+            else if(msg->command == NULL)
             {
                 *raw = '\0';
-                msg->user = pivot;
+                msg->command = pivot;
                 pivot = raw + 1;
             }
-            else if(*raw == ' ')
+            else
             {
-                if(msg->host == NULL)
-                {
-                    *raw = '\0';
-                    msg->host = pivot;
-                    pivot = raw + 1;
-                }
-                else if(msg->command == NULL)
-                {
-                    *raw = '\0';
-                    msg->command = pivot;
-                    pivot = raw + 1;
-                }
-                else
-                {
-                    *raw = '\0';
-                    msg->params[msg->paramc++] = pivot;
-                    pivot = raw + 1;
-                }
+                *raw = '\0';
+                msg->params[msg->paramc++] = pivot;
+                pivot = raw + 1;
             }
         }
-        
-        *raw = '\0';
     }
-    // Exposed functions //
     
-    int twitch_connect(twitch_conn* conn,char* name,char* oauthpass)
-    {
+    *raw = '\0';
+}
+// Exposed functions //
+
+int twitch_connect(twitch_conn* conn,char* name,char* oauthpass)
+{
 #ifdef _WIN32
-        WSADATA d;
-        WSAStartup(MAKEWORD(2,2), &d);
+    WSADATA d;
+    WSAStartup(MAKEWORD(2,2), &d);
 #endif      
-        
-        struct addrinfo *result = NULL;
-        struct addrinfo *ptr = NULL;
-        struct addrinfo hints = { 0 };
-        int iresult;
-        
-        hints.ai_family = AF_INET;
-        hints.ai_socktype = SOCK_STREAM;
-        
-        iresult = getaddrinfo(TWITCH_CHAT_API_ROOT, TWITCH_CHAT_API_PORT, &hints, &result);
-        if(iresult != 0 ) 
-        {
-            return 1;
-        }
-        
-        for(ptr = result; ptr != NULL; ptr = ptr->ai_next) {
-            conn->sock = (int)socket( ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
-            if(conn->sock == -1) 
-            {
-                return 2;
-            }
-            
-            iresult = connect(conn->sock, ptr->ai_addr, (int)ptr->ai_addrlen);
-            if(iresult < 0)
-            {
-                closesock(conn->sock);
-                continue;
-            }
-            break;
-        }
-        
-        freeaddrinfo(result);
-        
+    
+    struct addrinfo *result = NULL;
+    struct addrinfo *ptr = NULL;
+    struct addrinfo hints = { 0 };
+    int iresult;
+    
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    
+    iresult = getaddrinfo(TWITCH_CHAT_API_ROOT, TWITCH_CHAT_API_PORT, &hints, &result);
+    if(iresult != 0 ) 
+    {
+        return 1;
+    }
+    
+    for(ptr = result; ptr != NULL; ptr = ptr->ai_next) {
+        conn->sock = (int)socket( ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
         if(conn->sock == -1) 
         {
-            closesock(conn->sock);
-            return 3;
+            return 2;
         }
         
-        char* seek;
-        
-        
-        // Send oauth password
-        seek = conn->outbuffer;
-        seek = append_msg(seek, "PASS ");
-        seek = append_msg(seek, oauthpass);
-        seek = finalize_msg(seek);
-        sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
-        
-        // Send username
-        strcpy(conn->name,name);
-        seek = conn->outbuffer;
-        seek = append_msg(seek, "NICK ");
-        seek = append_msg(seek, name);
-        seek = finalize_msg(seek);
-        sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
-        
-        conn->isConnected = 1;
+        iresult = connect(conn->sock, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if(iresult < 0)
+        {
+            closesock(conn->sock);
+            continue;
+        }
+        break;
+    }
+    
+    freeaddrinfo(result);
+    
+    if(conn->sock == -1) 
+    {
+        closesock(conn->sock);
+        return 3;
+    }
+    
+    char* seek;
+    
+    
+    // Send oauth password
+    seek = conn->outbuffer;
+    seek = append_msg(seek, "PASS ");
+    seek = append_msg(seek, oauthpass);
+    seek = finalize_msg(seek);
+    sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+    
+    // Send username
+    strcpy(conn->name,name);
+    seek = conn->outbuffer;
+    seek = append_msg(seek, "NICK ");
+    seek = append_msg(seek, name);
+    seek = finalize_msg(seek);
+    sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+    
+    conn->isConnected = 1;
+    return 0;
+}
+int twitch_disconnect(twitch_conn* conn)
+{
+    conn->isConnected = 0;
+    closesock(conn->sock);
+#ifdef _WIN32
+    WSACleanup();
+#endif
+    return 0;
+}
+
+int twitch_joinchannel(twitch_conn* conn, char* channel)
+{
+    // set current channel name
+    strcpy(conn->curchannel.name,channel);
+    char* seek = conn->outbuffer;
+    seek = append_msg(seek, "JOIN #");
+    seek = append_msg(seek, channel);
+    seek = finalize_msg(seek);
+    
+    return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+}
+
+int twitch_leavechannel(twitch_conn* conn, char* channel)
+{
+    char* seek = conn->outbuffer;
+    seek = append_msg(seek, "PART #");
+    seek = append_msg(seek, channel);
+    seek = finalize_msg(seek);
+    
+    return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+}
+
+int twitch_sendmsg(twitch_conn* conn, char* msg)
+{
+    char* seek = conn->outbuffer;
+    seek = append_msg(seek, "PRIVMSG #");
+    seek = append_msg(seek, conn->curchannel.name);
+    seek = append_msg(seek, " :");
+    seek = append_msg(seek, msg);
+    seek = finalize_msg(seek);
+    
+    return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+}
+int twitch_sendmsgf(twitch_conn* conn,const char* format, ...)
+{
+    va_list vl;
+    va_start(vl,format);
+    char* seek = conn->outbuffer;
+    seek = append_msg(seek, "PRIVMSG #");
+    seek = append_msg(seek, conn->curchannel.name);
+    seek = append_msg(seek, " :");
+    seek += vsprintf(seek,format,vl);
+    seek = finalize_msg(seek);
+    
+    return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+}
+
+int twitch_sendraw(twitch_conn* conn, char* cmd)
+{
+    char* seek = conn->outbuffer;
+    seek = append_msg(seek, cmd);
+    return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+}
+int twitch_sendrawf(twitch_conn* conn, const char* format, ...)
+{
+    va_list vl;
+    va_start(vl,format);
+    int len = vsprintf(conn->outbuffer,format,vl);
+    return sendpak(conn->sock,conn->outbuffer,len);
+}
+int twitch_mainroutine(twitch_conn* conn)
+{
+    twitch_ircmessage ircmsg;
+    int result;
+    result = recvpak(conn->sock,conn->inbuffer,0x420);
+    // Nothing to process
+    if(result == 0)
+    {
         return 0;
     }
-    int twitch_disconnect(twitch_conn* conn)
+    //printf("recv: %s result=%d\n",conn->inbuffer,result);
+    // Error occured
+    if(result < 0)
     {
         conn->isConnected = 0;
-        closesock(conn->sock);
-#ifdef _WIN32
-        WSACleanup();
-#endif
+        return result;
+    }
+    //printf("recv: %s",conn->inbuffer);
+    // If this is a heartbeat request, just reply as is
+    if(!memcmp(conn->inbuffer,"PING",sizeof("PING")-1))
+    {
+        char* seek = conn->outbuffer;
+        seek = append_msg(seek,"PONG :tmi.twitch.tv\r\n");
+        sendpak(conn->sock,conn->outbuffer,sizeof("PONG :tmi.twitch.tv\r\n")-1);
         return 0;
     }
-    
-    int twitch_joinchannel(twitch_conn* conn, char* channel)
+    parse_irc(&ircmsg, conn->inbuffer);
+    if(conn->cmdrecvfn != NULL)
     {
-        // set current channel name
-        strcpy(conn->curchannel.name,channel);
-        char* seek = conn->outbuffer;
-        seek = append_msg(seek, "JOIN #");
-        seek = append_msg(seek, channel);
-        seek = finalize_msg(seek);
-        
-        return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+        conn->cmdrecvfn(&ircmsg);
     }
-    
-    int twitch_leavechannel(twitch_conn* conn, char* channel)
+    if(conn->msgrecvfn != NULL && 
+       !strcmp(ircmsg.command,"PRIVMSG") &&
+       ircmsg.paramc == 2)
     {
-        char* seek = conn->outbuffer;
-        seek = append_msg(seek, "PART #");
-        seek = append_msg(seek, channel);
-        seek = finalize_msg(seek);
-        
-        return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
+        conn->msgrecvfn(ircmsg.user,ircmsg.params[1]);
     }
-    
-    int twitch_sendmsg(twitch_conn* conn, char* msg)
-    {
-        char* seek = conn->outbuffer;
-        seek = append_msg(seek, "PRIVMSG #");
-        seek = append_msg(seek, conn->curchannel.name);
-        seek = append_msg(seek, " :");
-        seek = append_msg(seek, msg);
-        seek = finalize_msg(seek);
-        
-        return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
-    }
-    int twitch_sendmsgf(twitch_conn* conn,const char* format, ...)
-    {
-        va_list vl;
-        va_start(vl,format);
-        char* seek = conn->outbuffer;
-        seek = append_msg(seek, "PRIVMSG #");
-        seek = append_msg(seek, conn->curchannel.name);
-        seek = append_msg(seek, " :");
-        seek += vsprintf(seek,format,vl);
-        seek = finalize_msg(seek);
-        
-        return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
-    }
-    
-    int twitch_sendraw(twitch_conn* conn, char* cmd)
-    {
-        char* seek = conn->outbuffer;
-        seek = append_msg(seek, cmd);
-        return sendpak(conn->sock,conn->outbuffer,(size_t)(seek - conn->outbuffer));
-    }
-    int twitch_sendrawf(twitch_conn* conn, const char* format, ...)
-    {
-        va_list vl;
-        va_start(vl,format);
-        int len = vsprintf(conn->outbuffer,format,vl);
-        return sendpak(conn->sock,conn->outbuffer,len);
-    }
-    int twitch_mainroutine(twitch_conn* conn)
-    {
-        twitch_ircmessage ircmsg;
-        int result;
-        result = recvpak(conn->sock,conn->inbuffer,0x420);
-        // Nothing to process
-        if(result == 0)
-        {
-            return 0;
-        }
-        //printf("recv: %s result=%d\n",conn->inbuffer,result);
-        // Error occured
-        if(result < 0)
-        {
-            conn->isConnected = 0;
-            return result;
-        }
-        //printf("recv: %s",conn->inbuffer);
-        // If this is a heartbeat request, just reply as is
-        if(!memcmp(conn->inbuffer,"PING",sizeof("PING")-1))
-        {
-            char* seek = conn->outbuffer;
-            seek = append_msg(seek,"PONG :tmi.twitch.tv\r\n");
-            sendpak(conn->sock,conn->outbuffer,sizeof("PONG :tmi.twitch.tv\r\n")-1);
-            return 0;
-        }
-        parse_irc(&ircmsg, conn->inbuffer);
-        if(conn->cmdrecvfn != NULL)
-        {
-            conn->cmdrecvfn(&ircmsg);
-        }
-        if(conn->msgrecvfn != NULL && 
-           !strcmp(ircmsg.command,"PRIVMSG") &&
-           ircmsg.paramc == 2)
-        {
-            conn->msgrecvfn(ircmsg.user,ircmsg.params[1]);
-        }
-        return 0;
-    }
+    return 0;
+}
     
 #ifdef __cplusplus
 }
-    #endif
+#endif
--- twitch_api.h
+++ twitch_api.h
@@ -8,73 +8,73 @@ extern "C" {
 #endif
     
 #include <stdint.h>
+
+typedef struct twitch_ircmessage
+{
+    char* nick;
+    char* user;
+    char* host;
+    char* command;
+    int paramc;
+    char* params[16];
+    struct {
+        uint8_t isResponse : 1;
+    };
+} twitch_ircmessage;
+
+typedef void cmd_recv_fn_t(twitch_ircmessage* msg);
+typedef void msg_recv_fn_t(char* user, char* message);
+
+typedef struct twitch_viewer
+{
+    char name[0x20];
+    struct {
+        uint8_t isModerator : 1;
+        uint8_t isFollower : 1;
+        uint8_t isSubscribed : 1;
+    };
+} twitch_viewer;
+
+typedef struct twitch_channel
+{
+    char name[0x20];
+    twitch_viewer* viewers;
+    uint32_t viewercount;
+} twitch_channel;
+
+
+typedef struct twitch_conn
+{
+    char name[0x20];
+    twitch_channel curchannel;
     
-    typedef struct twitch_ircmessage
-    {
-        char* nick;
-        char* user;
-        char* host;
-        char* command;
-        int paramc;
-        char* params[16];
-        struct {
-            uint8_t isResponse : 1;
-        };
-    } twitch_ircmessage;
+    msg_recv_fn_t* msgrecvfn;
+    cmd_recv_fn_t* cmdrecvfn;
     
-    typedef void cmd_recv_fn_t(twitch_ircmessage* msg);
-    typedef void msg_recv_fn_t(char* user, char* message);
+    struct {
+        uint8_t isConnected : 1;
+    };
     
-    typedef struct twitch_viewer
-    {
-        char name[0x20];
-        struct {
-            uint8_t isModerator : 1;
-            uint8_t isFollower : 1;
-            uint8_t isSubscribed : 1;
-        };
-    } twitch_viewer;
-    
-    typedef struct twitch_channel
-    {
-        char name[0x20];
-        twitch_viewer* viewers;
-        uint32_t viewercount;
-    } twitch_channel;
-    
-    
-    typedef struct twitch_conn
-    {
-        char name[0x20];
-        twitch_channel curchannel;
-        
-        msg_recv_fn_t* msgrecvfn;
-        cmd_recv_fn_t* cmdrecvfn;
-        
-        struct {
-            uint8_t isConnected : 1;
-        };
-        
-        int sock;
-        // WARNING: Static buffer potentially unsafe.
-        char outbuffer[0x420];
-        char inbuffer[0x420];
-    } twitch_conn;
-    
-    
-    int twitch_connect(twitch_conn* conn,char* name,char* oauthpass);
-    int twitch_disconnect(twitch_conn* conn);
-    
-    int twitch_joinchannel(twitch_conn* conn, char* channel);
-    int twitch_leavechannel(twitch_conn* conn, char* channel);
-    
-    int twitch_sendraw(twitch_conn* conn, char* cmd);
-    int twitch_sendrawf(twitch_conn* conn, const char* format, ...);
-    
-    int twitch_sendmsg(twitch_conn* conn, char* msg);
-    int twitch_sendmsgf(twitch_conn* conn,const char* format, ...);
-    
-    int twitch_mainroutine(twitch_conn* conn);
+    int sock;
+    // WARNING: Static buffer potentially unsafe.
+    char outbuffer[0x420];
+    char inbuffer[0x420];
+} twitch_conn;
+
+
+int twitch_connect(twitch_conn* conn,char* name,char* oauthpass);
+int twitch_disconnect(twitch_conn* conn);
+
+int twitch_joinchannel(twitch_conn* conn, char* channel);
+int twitch_leavechannel(twitch_conn* conn, char* channel);
+
+int twitch_sendraw(twitch_conn* conn, char* cmd);
+int twitch_sendrawf(twitch_conn* conn, const char* format, ...);
+
+int twitch_sendmsg(twitch_conn* conn, char* msg);
+int twitch_sendmsgf(twitch_conn* conn,const char* format, ...);
+
+int twitch_mainroutine(twitch_conn* conn);
     
 #ifdef __cplusplus
 }
